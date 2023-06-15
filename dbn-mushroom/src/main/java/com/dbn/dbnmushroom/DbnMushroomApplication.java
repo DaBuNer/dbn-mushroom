6f23057cf7feebcb92afa6e9d00f0aed98213946
仰天长啸两三声 <1614542986@qq.com>
2023-06-15 17:21:53 +0800

    项目搭建

---
+++ dbn-mushroom/src/main/java/com/dbn/dbnmushroom/DbnMushroomApplication.java
@@ -0,0 +1,14 @@
+package com.dbn.dbnmushroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbnMushroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbnMushroomApplication.class, args);
+
+	}
+
+}
---
+++ dbn-mushroom/src/test/java/com/dbn/dbnmushroom/DbnMushroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbn.dbnmushroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbnMushroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
